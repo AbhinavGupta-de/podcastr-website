7166a3cb91deb47ee2d9c4967233a459b1305bad
Abhinav Gupta <Abhinav.gupta@outlook.de>
2024-06-13 12:59:20 +0530

    feat: basic routing and setup doen

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,17 @@
+import LeftSidebar from '@/components/LeftSidebar';
+
+export default function RootLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
+	return (
+		<div className="relative flex flex-col">
+			<main className="relative flex bg-black-3">
+				<LeftSidebar />
+				{children}
+				RIGHT SIDEBAR
+			</main>
+		</div>
+	);
+}
